bd371f4a172fb620bb5fb0ecb86fa9c36dc6424a
dimkary <43819376+dimkary@users.noreply.github.com>
2019-01-07 21:19:36 +0100

    Create timeloop.py

---
+++ Kattis/timeloop.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 20 09:49:20 2018
+@author: karips
+Solves the "Stuck In A Time Loop" Kattis problem
+"""
+x = input()
+for i in range(int(x)):
+    print(str(i+1) + ' Abracadabra')
