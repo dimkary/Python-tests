959e5f5588ecf1dc6f2032b4b861f1ff24189cf0
dimkary <43819376+dimkary@users.noreply.github.com>
2019-01-08 12:58:54 +0100

    Create carrots.py

---
+++ Kattis/carrots.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 20 09:49:20 2018
+@author: karips
+Solves the "Solving for Carrots" Kattis problem
+"""
+x = input().split(' ')
+nums = [int(i) for i in x]
+for i in range(nums[0]):
+    input()   
+print((nums[-1]))
