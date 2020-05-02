c2bde664464f9a9df3102e45e828e323fdc5710d
dimkary <43819376+dimkary@users.noreply.github.com>
2020-05-02 13:36:47 +0200

    Update cash.py

--- cash.py
+++ cash.py
@@ -40,3 +40,10 @@ def countChange(cash, coins):
     
     
 combos = countChange(cash,coins)
+
+#DP ALTERNATIVE 
+#s = [1] + [0]*200
+#for coin in [1, 2, 5, 10, 20, 50, 100, 200]:
+#    for i in range(len(s) - coin):
+#        s[i+coin] += s[i]
+#print(s[-1])
